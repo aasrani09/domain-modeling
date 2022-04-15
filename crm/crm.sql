2b5bebf14b10bce8ca1d3d82c33c9de94f629f53
aasrani09 <aarti.asrani@kellogg.northwestern.edu>
2022-04-15 21:15:59 +0000

    completed Class 3

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,42 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activity;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS salesforce;
+DROP TABLE IF EXISTS industries;
+
 -- CREATE TABLES
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    title TEXT,
+    phone TEXT,
+    company_id INTEGER);
+
+CREATE TABLE activity (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+   contact_id INTEGER,
+   salesforce_id INTEGER,
+    comm_type TEXT,
+    time TEXT,
+    notes TEXT);
+
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    company_name TEXT,
+    industry_id INTEGER); 
+
+CREATE TABLE salesforce (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    first_name TEXT,
+    last_name TEXT, 
+    email TEXT);
+
+CREATE TABLE industries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT,
+    company_id INTEGER);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,36 @@
 
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS likes;
+
+
+CREATE TABLE posts (
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+image_file TEXT,
+user_id INTEGER,
+time TEXT);
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    username TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    location TEXT);
+
+CREATE TABLE followers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    username_id TEXT, 
+    follower_username_id TEXT);
+
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    post_id INTEGER,
+    user_id INTEGER,
+    comment TEXT); 
+
+CREATE TABLE likes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    post_id INTEGER,
+    user_id INTEGER);
